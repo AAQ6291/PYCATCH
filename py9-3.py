b50d58e166c773b1d87b4383ee9c3ffe0651b845
angela_kuo <angelak.tw@gmail.com>
2018-08-16 17:59:13 +0800

    20180816-2nd

--- py9-3.py
+++ py9-3.py
@@ -1,9 +1,16 @@
 # 擷取網頁資料內的EMAIL帳號
 
 import requests
+import re  # 正則表達式的模組
 
-url = 'https://www.com.tw/cross/namequery107.html'
-html = requests.get(url).text.splitlines()
-# 截取前15行
-for i in range(0, 55):
-    print(html[i])
+regex = r"([a-zA-Z0-9_.+-]+@[a-pr-zA-PRZ0-9-]+\.[a-zA-Z0-9-.]+)"
+# 基于隐私，使用了“XXXXXXXXXXXXXX”
+url = 'http://www.liveabc.com/index.asp'
+html = requests.get(url).text
+# print(html)
+emails = re.findall(regex, html)
+i = 0
+for email in emails:
+    i += 1
+    if i < 16:
+        print("{} :{}".format(i, email))
